a62b8ccc9ceb3dfc90910a03003216f69a4e0e88
Distortion <c.hanqi@wustl.edu>
2023-09-11 04:45:57 -0500

    AWS EC2 port

--- .history/vite.config_20230910175218.ts
+++ .history/vite.config_20230910175218.ts
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // AWS EC2 port choose
-  server: {
-    host: '0.0.0.0', // Bind to all network interfaces
-    port: 80, // Use port 80 for HTTP
-  },
 })
